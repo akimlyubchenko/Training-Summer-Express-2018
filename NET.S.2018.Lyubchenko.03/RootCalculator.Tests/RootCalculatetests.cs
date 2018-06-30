2d2e0637469b8c8c90bfb2c69a98f424b9930cef
Akim <akim.lyubchenko@gmail.com>
2018-06-30 20:14:00 +0300

    RootCalculator

---
+++ NET.S.2018.Lyubchenko.03/RootCalculator.Tests/RootCalculatetests.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace RootCalculaor.Tests
+{
+    [TestClass]
+    public class RootCalculatetests
+    {
+        [TestMethod]
+        public void FindNthRoot_1_5_00001_1()
+        {
+            double number = 1;
+            double degree = 5;
+            double precision = 0.0001;
+            double expected = 1;
+
+            double actual = RootCalculator.RootCalculator.FindNthRoot(number, degree, precision);
+
+            Assert.AreEqual(expected, actual, precision);
+        }
+
+        [TestMethod]
+        public void FindNthRoot_8_3_00001_2()
+        {
+            double number = 8;
+            double degree = 3;
+            double precision = 0.0001;
+            double expected = 2;
+
+            double actual = RootCalculator.RootCalculator.FindNthRoot(number, degree, precision);
+
+            Assert.AreEqual(expected, actual, precision);
+        }
+
+        [TestMethod]
+        public void FindNthRoot_0001_3_00001_2()
+        {
+            double number = 0.001;
+            double degree = 3;
+            double precision = 0.0001;
+            double expected = 0.1;
+
+            double actual = RootCalculator.RootCalculator.FindNthRoot(number, degree, precision);
+
+            Assert.AreEqual(expected, actual, precision);
+        }
+
+        [TestMethod]
+        public void FindNthRoot_004100625_4_00001_045()
+        {
+            double number = 0.04100625;
+            double degree = 4;
+            double precision = 0.0001;
+            double expected = 0.45;
+
+            double actual = RootCalculator.RootCalculator.FindNthRoot(number, degree, precision);
+
+            Assert.AreEqual(expected, actual, precision);
+        }
+
+        [TestMethod]
+        public void FindNthRoot_27_3_00001_3()
+        {
+            double number = 27;
+            double degree = 3;
+            double precision = 0.0001;
+            double expected = 3;
+
+            double actual = RootCalculator.RootCalculator.FindNthRoot(number, degree, precision);
+
+            Assert.AreEqual(expected, actual, precision);
+        }
+
+        [TestMethod]
+        public void FindNthRoot_Negative0008_3_01_Negative02()
+        {
+            double number = -0.008;
+            int degree = 3;
+            double precision = 0.01;
+            double expected = -0.2;
+
+            double actual = RootCalculator.RootCalculator.FindNthRoot(number, degree, precision);
+
+            Assert.AreEqual(expected, actual, precision);
+        }
+
+        [TestMethod]
+        public void FindNthRoot_0004241979_9_000000001_0545()
+        {
+            double number = 0.004241979;
+            double degree = 9;
+            double precision = 0.00000001;
+            double expected = 0.545;
+
+            double actual = RootCalculator.RootCalculator.FindNthRoot(number, degree, precision);
+
+            Assert.AreEqual(expected, actual, precision);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void FindNthRoot_NegativeNumberEvenDegree_Exception()
+            => RootCalculator.RootCalculator.FindNthRoot(-1, 6);
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void FilterNthRoot_NegativeDegree_Exception()
+            => RootCalculator.RootCalculator.FindNthRoot(2, -4);
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void FilterNthRoot_NegativePrecision_Exception()
+            => RootCalculator.RootCalculator.FindNthRoot(2,2,-2);
+    }
+}
---
+++ NET.S.2018.Lyubchenko.03/RootCalculator/RootCalculator.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace RootCalculator
+{
+    /// <summary>
+    /// Calculates root n of the number
+    /// </summary>
+    public class RootCalculator
+    {
+        #region Calculates root n of the number
+        /// <summary>
+        /// Calculates root n of the number
+        /// </summary>
+        /// <param name="number"> Using number</param>
+        /// <param name="degree"> Degree of root </param>
+        /// <param name="precision"> Precision </param>
+        /// <returns> Finished number </returns>
+        public static double FindNthRoot(double number, double degree, double precision = 0.0000001)
+        {
+            Exceptions(number, degree, precision);
+            double x0 = number / degree;
+            double x = Equation(number, degree, x0);
+
+            while (Math.Abs(x - x0) > precision)
+            {
+                x0 = x;
+                x = Equation(number, degree, x0);
+            }
+
+            return x;
+        }
+        #endregion
+        #region Raises to the power of number
+        /// <summary>
+        /// Raises to the power of number
+        /// </summary>
+        /// <param name="number"> Using number </param>
+        /// <param name="pow"> Value of a power</param>
+        /// <returns> Finished number</returns>
+        private static double Pow(double number, int pow)
+        {
+            double resultExponentiation = 1;
+            for (int i = 0; i < pow; i++)
+            {
+                resultExponentiation *= number;
+            }
+
+            return resultExponentiation;
+        }
+        #endregion
+        #region Large formula
+        /// <summary>
+        /// It's large formula
+        /// </summary>
+        /// <param name="number"> Number </param>
+        /// <param name="degree"> Degree </param>
+        /// <param name="x0"> Last x </param>
+        /// <returns> Result of the formula </returns>
+        private static double Equation(double number, double degree, double x0)
+        {
+            double x = (1 / degree) * (((degree - 1) * x0) + (number / Pow(x0, (int)degree - 1)));
+            return x;
+        }
+        #endregion
+        #region The set of exceptions
+        /// <summary>
+        /// The set of exceptions
+        /// </summary>
+        /// <param name="number"> Number </param>
+        /// <param name="degree"> Degree </param>
+        /// <param name="precision"> Precision </param>
+        /// <exception cref="ArgumentException"> 
+        /// 3 types exceptions 
+        /// </exception>
+        private static void Exceptions(double number, double degree, double precision)
+        {
+            if (number < 0 && degree % 2 == 0)
+            {
+                throw new ArgumentException($"Number {number} and degree {degree} isn't valid");
+            }
+
+            if (degree < 0)
+            {
+                throw new ArgumentException($"Degree {degree} isn't valid");
+            }
+
+            if (precision < 0)
+            {
+                throw new ArgumentException($"Precision {precision} isn't valid");
+            }
+        }
+        #endregion
+    }
+}
