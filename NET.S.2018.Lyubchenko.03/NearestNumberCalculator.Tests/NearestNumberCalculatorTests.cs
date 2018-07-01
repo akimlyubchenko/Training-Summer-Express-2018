76f787755ee2a927aee7b0593154410421f47179
Akim <akim.lyubchenko@gmail.com>
2018-07-01 18:11:17 +0300

    NearestNumberCalculator

---
+++ NET.S.2018.Lyubchenko.03/NearestNumberCalculator.Tests/NearestNumberCalculatorTests.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static NearestNumberCalculator.NearestNumberCalculator;
+
+namespace NearestNumberCalculatorTests
+{
+    [TestClass]
+    public class NearestNumberCalculatorTests
+    {
+        [TestMethod]
+        public void NearestNumberCalculator_123456_6()
+        {
+            // Arr
+            int number = 1234126;
+            int expected = 1234162;
+
+            // Act
+            int actual = FindNextBiggerNumber(number);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void NearestNumberCalculator_12_21()
+        {
+            // Arr
+            int number = 12;
+            int expected = 21;
+
+            // Act
+            int actual = FindNextBiggerNumber(number);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void NearestNumberCalculator_513_531()
+        {
+            // Arr
+            int number = 513;
+            int expected = 531;
+
+            // Act
+            int actual = FindNextBiggerNumber(number);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void NearestNumberCalculator_2017_2071()
+        {
+            // Arr
+            int number = 2017;
+            int expected = 2071;
+
+            // Act
+            int actual = FindNextBiggerNumber(number);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void NearestNumberCalculator_414_441()
+        {
+            // Arr
+            int number = 414;
+            int expected = 441;
+
+            // Act
+            int actual = FindNextBiggerNumber(number);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void NearestNumberCalculator_144_414()
+        {
+            int number = 144;
+            int expected = 414;
+
+            // Act
+            int actual = FindNextBiggerNumber(number);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void NearestNumberCalculator_1234321_1241233()
+        {
+            // Arr
+            int number = 1234321;
+            int expected = 1241233;
+
+            // Act
+            int actual = FindNextBiggerNumber(number);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void NearestNumberCalculator_1234126_1234162()
+        {
+            // Arr
+            int number = 1234126;
+            int expected = 1234162;
+
+            // Act
+            int actual = FindNextBiggerNumber(number);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void NearestNumberCalculator_3456432_3462345()
+        {
+            // Arr
+            int number = 3456432;
+            int expected = 3462345;
+
+            // Act
+            int actual = FindNextBiggerNumber(number);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void NearestNumberCalculator_10_Negative1()
+        {
+            // Arr
+            int number = 10;
+            int expected = -1;
+
+            // Act
+            int actual = FindNextBiggerNumber(number);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void NearestNumberCalculator_987654321_Negative1()
+        {
+            // Arr
+            int number = 987654321;
+            int expected = -1;
+
+            // Act
+            int actual = FindNextBiggerNumber(number);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void NearestNumberCalculator_NegativeNumber_Exception()
+            => FindNextBiggerNumber(-1);
+    }
+}
---
+++ NET.S.2018.Lyubchenko.03/NearestNumberCalculator/NearestNumberCalculator.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NearestNumberCalculator
+{
+    /// <summary>
+    /// Finder next bigger number
+    /// </summary>
+    public class NearestNumberCalculator
+    {
+        #region Finder next bigger number
+        /// <summary>
+        /// Finder next bigger number
+        /// </summary>
+        /// <param name="number"> The original number </param>
+        /// <returns> Final number </returns>
+        public static int FindNextBiggerNumber(int number)
+        {
+            Exception(number);
+            int digitCount = (int)Math.Log10(number) + 1;
+            int[] array = new int[digitCount];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[array.Length - i - 1] = number % 10;
+                number /= 10;
+            }
+
+            for (int i = array.Length - 1; i > 0; i--)
+            {
+                if (array[i - 1] < array[i])
+                {
+                    Swap(ref array[i - 1], ref array[i]);
+                    Quicksort(ref array, i, array.Length - 1);
+                    for (int j = 0; j < array.Length; j++)
+                    {
+                        number += array[j];
+                        if (j != array.Length - 1)
+                        {
+                            number *= 10;
+                        }
+                    }
+
+                    return number;
+                }
+            }
+
+            return -1;
+        }
+        #endregion
+
+        #region QuickSort definition
+        /// <summary>
+        /// Accepts an array for further processing
+        /// </summary>
+        /// <param name="array"> Input array </param>
+        /// <param name="start"> Input start index </param>
+        /// <param name="end"> Input last index </param>
+        /// <returns> The right array </returns>
+        public static void Quicksort(ref int[] array, int start, int end)
+        {
+            if (start < end)
+            {
+                int pivot = Partition(array, start, end);
+                if (pivot > 1)
+                {
+                    Quicksort(ref array, start, pivot - 1);
+                }
+
+                if (pivot + 1 < end)
+                {
+                    Quicksort(ref array, pivot + 1, end);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Sort part between start and end
+        /// </summary>
+        /// <param name="array">Input array</param>
+        /// <param name="start">Input start index</param>
+        /// <param name="end">Input last index</param>
+        /// <returns> Index of partition </returns>
+        private static int Partition(int[] array, int start, int end)
+        {
+            int marker = start;
+            for (int i = start; i <= end; i++)
+            {
+                if (array[i] < array[end])
+                {
+                    Swap(ref array[marker], ref array[i]);
+                    marker++;
+                }
+            }
+
+            Swap(ref array[marker], ref array[end]);
+            return marker;
+        }
+        #endregion
+
+        /// <summary>
+        /// Swap 2 integer elements
+        /// </summary>
+        /// <param name="one"> It's first element </param>
+        /// <param name="two"> It's second element </param>
+        private static void Swap(ref int one, ref int two)
+        {
+            int temp;
+            temp = one;
+            one = two;
+            two = temp;
+        }
+        #region If number is valid
+        /// <summary>
+        /// If number is valid
+        /// </summary>
+        /// <param name="number"> Number </param>
+        /// <exception cref="ArgumentException"> 
+        /// number less than 1 
+        /// </exception>
+        private static void Exception(double number)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentException($"Number {number} is negative");
+            }
+        }
+        #endregion
+    }
+}
