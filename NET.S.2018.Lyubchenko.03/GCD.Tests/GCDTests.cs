8fd312ef906c5d8ac7cbb84e33041220cf673744
Akim <akim.lyubchenko@gmail.com>
2018-07-02 22:36:29 +0300

    Cleaned repositories and added GCD

---
+++ NET.S.2018.Lyubchenko.03/GCD.Tests/GCDTests.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GCD.Tests
+{
+    [TestClass]
+    public class GCDTests
+    {
+        [TestMethod]
+        public void GetGCD_800And225_25()
+        {
+            // Arr
+            int v1 = 800, v2 = 225;
+            int expected = 25;
+
+            // Act
+            int actual = GCD.GetGCD(v1, v2);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetGCD_0And25_25()
+        {
+            // Arr
+            int v1 = 0, v2 = 25;
+            int expected = 25;
+
+            // Act
+            int actual = GCD.GetGCD(v1, v2);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetGCD_40And0_40()
+        {
+            // Arr
+            int v1 = 40, v2 = 0;
+            int expected = 40;
+
+            // Act
+            int actual = GCD.GetGCD(v1, v2);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetGCD_1000And225_100()
+        {
+            // Arr
+            int v1 = 1000, v2 = 100;
+            int expected = 100;
+
+            // Act
+            int actual = GCD.GetGCD(v1, v2);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetGCD_750And250_250()
+        {
+            // Arr
+            int v1 = 750, v2 = 250;
+            int expected = 250;
+
+            // Act
+            int actual = GCD.GetGCD(v1, v2);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetGCD_345720And250_2345()
+        {
+            // Arr
+            int v1 = 345720, v2 = 2345;
+            int expected = 335;
+
+            // Act
+            int actual = GCD.GetGCD(v1, v2);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetGCD_Negative750And250_Exception()
+            => GCD.GetGCD(-750, 250);
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetGCD_50AndNegative40_Exception()
+           => GCD.GetGCD(50, -40);
+
+        [TestMethod]
+        public void GetGCD_6And9And15_3()
+        {
+            // Arr
+            int v1 = 6, v2 = 9, v3 = 15;
+            int expected = 3;
+
+            // Act
+            int actual = GCD.GetGCD(v1, v2, v3);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetGCD_64And32And128_32()
+        {
+            // Arr
+            int v1 = 64, v2 = 32, v3 = 128;
+            int expected = 32;
+
+            // Act
+            int actual = GCD.GetGCD(v1, v2, v3);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetGCD_64And32And128And30And12And8_2()
+        {
+            // Arr
+            int v1 = 64, v2 = 32, v3 = 128, v4 = 30, v5 = 12, v6 = 8;
+            int expected = 2;
+
+            // Act
+            int actual = GCD.GetGCD(v1, v2, v3, v4, v5, v6);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetGCD_64And32And128And30AndNegative12And8_Exception()
+            => GCD.GetGCD(64, 42, 128, 30, -12, 8);
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetGCD_5_Exception()
+            => GCD.GetGCD(5);
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetGCD_Empty_Exception()
+            => GCD.GetGCD();
+
+        [TestMethod]
+        public void GetGCD_64And32And0_32()
+        {
+            // Arr
+            int v1 = 64, v2 = 32, v3 = 0;
+            int expected = 32;
+
+            // Act
+            int actual = GCD.GetGCD(v1, v2, v3);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetGCD_64And32And1_32()
+        {
+            // Arr
+            int v1 = 64, v2 = 32, v3 = 128;
+            int expected = 32;
+
+            // Act
+            int actual = GCD.GetGCD(v1, v2, v3);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetGCD_11And56And12346_1()
+        {
+            // Arr
+            int v1 = 11, v2 = 56, v3 = 12346;
+            int expected = 1;
+
+            // Act
+            int actual = GCD.GetGCD(v1, v2, v3);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetSteinGCD_10And5_5()
+        {
+            // Arr
+            int v1 = 10, v2 = 5;
+            int expected = 5;
+
+            // Act
+            int actual = GCD.GetGCD(v1, v2);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetSteinGCD_16And20_4()
+        {
+            // Arr
+            int v1 = 16, v2 = 20;
+            int expected = 4;
+
+            // Act
+            int actual = GCD.GetGCD(v1, v2);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetSteinGCD_16And5And1234_1()
+        {
+            // Arr
+            int v1 = 16, v2 = 5, v3 = 1234;
+            int expected = 1;
+
+            // Act
+            int actual = GCD.GetGCD(v1, v2, v3);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetSteinGCD_16And2000And1234_1()
+        {
+            // Arr
+            int v1 = 16, v2 = 2000, v3 = 1234;
+            int expected = 2;
+
+            // Act
+            int actual = GCD.GetGCD(v1, v2, v3);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetSteinGCD_16And0And1234_1()
+        {
+            // Arr
+            int v1 = 16, v2 = 0, v3 = 1234;
+            int expected = 2;
+
+            // Act
+            int actual = GCD.GetGCD(v1, v2, v3);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetSteinGCD_64And32And128And30AndNegative12And8_Exception()
+            => GCD.GetGCD(64, 42, 128, 30, -12, 8);
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetSteinGCD_5_Exception()
+            => GCD.GetGCD(5);
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetSteinGCD_Empty_Exception()
+            => GCD.GetGCD();
+
+        [TestMethod]
+        public void TimingEuclidean_Watch()
+        {
+            Debug.Write($"Spent time: {GCD.TimingEuclidean()}");
+        }
+
+        [TestMethod]
+        public void TimingStein_Watch()
+        {
+            Debug.Write($"Spent time: {GCD.TimingStein()}");
+        }
+
+    }
+}
---
+++ NET.S.2018.Lyubchenko.03/GCD/GCD.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace GCD
+{
+    /// <summary>
+    /// Finder greatest common divisor in array of numbers
+    /// </summary>
+    public class GCD
+    {
+        #region Euclidean algorithm
+        /// <summary>
+        /// Starts the search GCD(Euclidean algorithm)
+        /// </summary>
+        /// <param name="numbers"> Used numbers </param>
+        /// <returns> GCD </returns>
+        public static int GetGCD(params int[] numbers)
+        {
+            IsValid(numbers);
+            int gcd = GetGCDTwoNumbers(numbers[0], numbers[1]);
+            for (int i = 2; i < numbers.Length; i++)
+            {
+                gcd = GetGCDTwoNumbers(gcd, numbers[i]);
+                if (gcd == 1)
+                {
+                    return gcd;
+                }
+            }
+
+            return gcd;
+        }
+
+        /// <summary>
+        /// Particular cases and order of numbers
+        /// </summary>
+        /// <param name="value1"> First number </param>
+        /// <param name="value2"> Second number </param>
+        /// <returns> GCD </returns>
+        private static int GetGCDTwoNumbers(int value1, int value2)
+        {
+            int answer = -1;
+            if (value1 > value2)
+            {
+                answer = GCDCalculator(value1, value2);
+            }
+            else if (value2 > value1)
+            {
+                answer = GCDCalculator(value2, value1);
+            }
+            else if (value1 == value2)
+            {
+                answer = value1;
+            }
+
+            return answer;
+        }
+
+        /// <summary>
+        ///  GCD search algorithm(Euclidean)
+        /// </summary>
+        /// <param name="value1"> First number </param>
+        /// <param name="value2"> Second number </param>
+        /// <returns> GCD </returns>
+        private static int GCDCalculator(int value1, int value2)
+        {
+            if (value2 != 0)
+            {
+                int temp = value1 / value2;
+                if (temp >= 1)
+                {
+                    value1 = value1 - (value2 * temp);
+                    value1 = GCDCalculator(value2, value1);
+                }
+
+                return value1;
+            }
+            else
+            {
+                return value1;
+            }
+        }
+        #endregion
+        #region Stein's algorithm
+        /// <summary>
+        /// Starts the search GCD(Stein's algorithm)
+        /// </summary>
+        /// <param name="numbers"> Used numbers </param>
+        /// <returns> GCD </returns>
+        public static int GetSteinGCD(params int[] numbers)
+        {
+            IsValid(numbers);
+            int gcd = SteinGCD(numbers[0], numbers[1]);
+            for (int i = 2; i < numbers.Length; i++)
+            {
+                gcd = SteinGCD(gcd, numbers[i]);
+                if (gcd == 1)
+                {
+                    return gcd;
+                }
+            }
+
+            return gcd;
+        }
+
+        /// <summary>
+        ///  GCD search algorithm(Stein's algorithm)
+        /// </summary>
+        /// <param name="value1"> First number </param>
+        /// <param name="value2"> Second number </param>
+        /// <returns> GCD </returns>
+        private static int SteinGCD(int value1, int value2)
+        {
+            if (value1 == value2)
+            {
+                return value1;
+            }
+
+            if (value1 == 0)
+            {
+                return value2;
+            }
+
+            if (value2 == 0)
+            {
+                return value1;
+            }
+
+            if ((~value1 & 1) != 0)
+            {
+                if ((value2 & 1) != 0)
+                {
+                    return SteinGCD(value1 >> 1, value2);
+                }
+                else
+                {
+                    return SteinGCD(value1 >> 1, value2 >> 1) << 1;
+                }
+            }
+
+            if ((~value2 & 1) != 0)
+            {
+                return SteinGCD(value1, value2 >> 1);
+            }
+
+            if (value1 > value2)
+            {
+                return SteinGCD((value1 - value2) >> 1, value2);
+            }
+
+            return SteinGCD((value2 - value1) >> 1, value1);
+        }
+        #endregion
+        #region Timing
+        /// <summary>
+        /// Time of Euclidean algorithm execution
+        /// </summary>
+        /// <returns> Time </returns>
+        public static string TimingEuclidean()
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            GetGCD(2226, 4256, 44444444, 444444444, 44444444, 666666666, 888888, 666666, 4222, 1246, 846466);
+            stopWatch.Stop();
+            TimeSpan timespan = stopWatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}.{4:00}",
+                timespan.Hours, timespan.Minutes, timespan.Seconds,
+                timespan.Milliseconds ,timespan.Ticks);
+            return elapsedTime;
+        }
+
+        /// <summary>
+        /// Time of Stein's algorithm execution
+        /// </summary>
+        /// <returns> Time </returns>
+        public static string TimingStein()
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            GetSteinGCD(15, 33, 9, 345, 345, 335345356, 123123, 5675858, 21324235, 23465);
+            stopWatch.Stop();
+            TimeSpan timespan = stopWatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}.{4:00}",
+                timespan.Hours, timespan.Minutes, timespan.Seconds,
+                timespan.Milliseconds, timespan.Ticks);
+            return elapsedTime;
+        }
+        #endregion
+
+        /// <summary>
+        /// Checked if array is valid(number[i] must be > 0 , numbers.Length !=0 !=1 )
+        /// </summary>
+        /// <param name="numbers"> Used numbers </param>
+        /// <exception cref="ArgumentException">
+        /// Thrown when the number less than  0 or length of array = 1 or = 0
+        /// </exception>
+        private static void IsValid(int[] numbers)
+        {
+            if (numbers.Length == 0 || numbers.Length == 1)
+            {
+                throw new ArgumentException($"Enter at least 2 numbers");
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] < 0)
+                {
+                    throw new ArgumentException($"Value {numbers[i]} mustn't be negative");
+                }
+            }
+        }
+    }
+}
