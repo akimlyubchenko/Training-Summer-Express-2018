afbda0f248a2f8b5bd172f3c8a64c9a0dd1f083b
Akim <akim.lyubchenko@gmail.com>
2018-07-07 14:39:42 +0300

    Add ToDecimal

---
+++ NET.S.2018.Lyubchenko.05.06/ToDecimal/ToDecimal.cs
@@ -0,0 +1,160 @@
+﻿using System;
+
+namespace ToDecimal
+{
+    /// <summary>
+    /// Converter number from any notation more 1 and less 17, to decimal number
+    /// </summary>
+    public static class ToDecimal
+    {
+        #region public API        
+        /// <summary>
+        /// Converter number from any notation more 1 and less 17, to decimal number
+        /// </summary>
+        /// <param name="numberString"> Entered number in string </param>
+        /// <param name="notation"> The notation </param>
+        /// <returns> Integer decimal number</returns>
+        public static int ToDecimalConverter(string numberString, int notation)
+        {
+            IsValid(numberString, notation);
+            int[] numbers = StringIntConvert(numberString, notation);
+            return DoneNumber(numbers, notation);
+        }
+        #endregion
+
+        #region private API        
+        /// <summary>
+        /// Works with a ready array of numbers
+        /// </summary>
+        /// <param name="numbers"> The ready array </param>
+        /// <param name="notation"> The notation </param>
+        /// <returns> A ready number </returns>
+        private static int DoneNumber(int[] numbers, int notation)
+        {
+            int number = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                number += numbers[i] * (int)Math.Pow(notation, numbers.Length - 1 - i);
+            }
+            return number;
+        }
+
+        /// <summary>
+        /// From string to int converter
+        /// </summary>
+        /// <param name="numberString"> Elementary string </param>
+        /// <param name="notation"> The notation </param>
+        /// <returns> Int array </returns>
+        private static int[] StringIntConvert(string numberString, int notation)
+        {
+            if (notation > 10)
+            {
+                int[] numbers = StringIntConvertMoreDecimal(numberString);
+                return numbers;
+            }
+            else
+            {
+                int[] numbers = new int[numberString.Length];
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    numbers[i] = numberString[i] - 48;
+                }
+
+                return numbers;
+            }
+        }
+
+        /// <summary>
+        /// From string to int converter width notation more as 10
+        /// </summary>
+        /// <param name="numberString"> Elementary string </param>
+        /// <returns> Int array </returns>
+        private static int[] StringIntConvertMoreDecimal(string numberString)
+        {
+            int[] numbers = new int[numberString.Length];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numberString[i] < 65)
+                {
+                    numbers[i] = numberString[i] - 48;
+                }
+                else if (numberString[i] == 65 || numberString[i] == 97)
+                {
+                    numbers[i] = 10;
+                }
+                else if (numberString[i] == 66 || numberString[i] == 98)
+                {
+                    numbers[i] = 11;
+                }
+                else if (numberString[i] == 67 || numberString[i] == 99)
+                {
+                    numbers[i] = 12;
+                }
+                else if (numberString[i] == 68 || numberString[i] == 100)
+                {
+                    numbers[i] = 13;
+                }
+                else if (numberString[i] == 69 || numberString[i] == 101)
+                {
+                    numbers[i] = 14;
+                }
+                else if (numberString[i] == 70 || numberString[i] == 102)
+                {
+                    numbers[i] = 15;
+                }
+            }
+
+            return numbers;
+        }
+
+        /// <summary>
+        /// Returns true if ... is valid.
+        /// </summary>
+        /// <param name="numberString"> Elementary string </param>
+        /// <param name="notation"> The notation </param>
+        /// <exception cref="ArgumentException">
+        /// Notation or number are invalid
+        /// </exception>
+        /// <exception cref="ArgumentNullException"> Elementary number isn't valid </exception>
+        private static void IsValid(string numberString, int notation)
+        {
+            if (notation < 2 || notation > 16)
+            {
+                throw new ArgumentException($"Notation is invalid");
+            }
+
+            if (numberString == null)
+            {
+                throw new ArgumentNullException(nameof(numberString));
+            }
+
+            for (int i = 0; i < numberString.Length; i++)
+            {
+                if (notation < 11)
+                {
+                    if (numberString[i] - 48 < 0 || numberString[i] - 48 > 9)
+                    {
+                        throw new ArgumentException($"Number is invalid");
+                    }
+                }
+                else
+                {
+                    if (numberString[i] >= 48 && numberString[i] <= 57)
+                    {
+                        continue;
+                    }
+                    else if (numberString[i] >= 65 && numberString[i] <= 70)
+                    {
+                        continue;
+                    }
+                    else if (numberString[i] >= 97 && numberString[i] <= 102)
+                    {
+                        continue;
+                    }
+                    throw new ArgumentException($"Number is invalid");
+                }
+            }
+        }
+        #endregion
+    }
+}
---
+++ NET.S.2018.Lyubchenko.05.06/ToDecimalTests/ToDecimalTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ToDecimal.Tests
+{
+    [TestFixture]
+    public class ToDecimalTests
+    {
+        [TestCase("0110111101100001100001010111111", 2, ExpectedResult = 934331071)]
+        public int ToDecimalConverter_0110111101100001100001010111111_2_934331071(string numberString, int notation)
+           => ToDecimal.ToDecimalConverter(numberString, notation);
+
+        [TestCase("01101111011001100001010111111", 2, ExpectedResult = 233620159)]
+        public int ToDecimalConverter_01101111011001100001010111111_2_233620159(string numberString, int notation)
+          => ToDecimal.ToDecimalConverter(numberString, notation);
+
+        [TestCase("1AeF101", 16, ExpectedResult = 28242177)]
+        public int ToDecimalConverter_1AeF101_16_28242177(string numberString, int notation)
+          => ToDecimal.ToDecimalConverter(numberString, notation);
+
+        [TestCase("1ACB67", 16, ExpectedResult = 1756007)]
+        public int ToDecimalConverter_1ACB67_16_1756007(string numberString, int notation)
+          => ToDecimal.ToDecimalConverter(numberString, notation);
+
+        [TestCase("764241", 8, ExpectedResult = 256161)]
+        public int ToDecimalConverter_764241_8_256161(string numberString, int notation)
+         => ToDecimal.ToDecimalConverter(numberString, notation);
+
+        [TestCase("10", 5, ExpectedResult = 5)]
+        public int ToDecimalConverter_10_5_5(string numberString, int notation)
+         => ToDecimal.ToDecimalConverter(numberString, notation);
+
+        [TestCase("1Aec101", 15, ExpectedResult = 19733851)]
+        public int ToDecimalConverter_1Aec101_15_19733851(string numberString, int notation)
+         => ToDecimal.ToDecimalConverter(numberString, notation);
+
+        [Test]
+        public void ToDecimalConverter_WithNull_ThrowArgumentNullException()
+           => Assert.Throws<ArgumentNullException>(() => ToDecimal.ToDecimalConverter(null,5));
+
+        [Test]
+        public void ToDecimalConverter_NotationLessAsNumber_ThrowArgumentException()
+          => Assert.Throws<ArgumentException>(() => ToDecimal.ToDecimalConverter("SA123", 2));
+
+        [Test]
+        public void ToDecimalConverter_NotationMoreAsNumber_ThrowArgumentException()
+         => Assert.Throws<ArgumentException>(() => ToDecimal.ToDecimalConverter("764241", 20));
+    }
+}
