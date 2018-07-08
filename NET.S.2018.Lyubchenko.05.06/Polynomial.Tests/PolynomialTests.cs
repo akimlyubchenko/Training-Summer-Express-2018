ff0ac6ad3360be7cb54c78018c4737a55709afd8
Akim <akim.lyubchenko@gmail.com>
2018-07-08 20:38:07 +0300

    Polynomial

---
+++ NET.S.2018.Lyubchenko.05.06/Polynomial.Tests/PolynomialTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Polynomial.Tests
+{
+    [TestFixture]
+    public class PolynomialTests
+    {
+        [TestCase(1, 2, 3, ExpectedResult = "1x^2 + 2x + 3 = 0")]
+        [TestCase(34.34, 21.42, 0, 1, ExpectedResult = "34,34x^3 + 21,42x^2 + 1 = 0")]
+        [TestCase(1, 123.3, 0, ExpectedResult = "1x^2 + 123,3x = 0")]
+        public string ToString_DoubleArray_ValidResult(params double[] coefs)
+        {
+            Polynomial pol = new Polynomial(coefs);
+            return pol.ToString();
+        }
+
+
+        [TestCase(1, 2, 3, ExpectedResult = 55467396)]
+
+        [Test]
+        public int HashCode_DoubleArray_ValidResult(params double[] coefs)
+        {
+            Polynomial pol = new Polynomial(coefs);
+            return pol.GetHashCode();
+        }
+
+        [TestCase(new double[] { 1, 2, 3 }, new double[] { 2, 4, 6 }, new double[] { 3, 6, 9 })]
+        [TestCase(new double[] { 14.5, 0, -5.5 }, new double[] { 22.5, 4, -4.5 }, new double[] { 37, 4, -10 })]
+        public void Addition_DoubleArrays_ValidAddition(double[] coefs1, double[] coefs2, double[] coefsex)
+        {
+            Polynomial pol1 = new Polynomial(coefs1);
+            Polynomial pol2 = new Polynomial(coefs2);
+            Polynomial polex = new Polynomial(coefsex);
+
+            Polynomial actual = pol1 + pol2;
+
+            Assert.True(polex == actual);
+
+        }
+
+        [TestCase(new double[] { 3, 6, 9 }, new double[] { 1, 2, 3 }, new double[] { 2, 4, 6 })]
+        [TestCase(new double[] { 37, 4, -10 }, new double[] { 14.5, 0, -5.5 }, new double[] { 22.5, 4, -4.5 })]
+        public void Subtraction_DoubleArrays_ValidSubtraction(double[] coefs1, double[] coefs2, double[] coefsex)
+        {
+            Polynomial pol1 = new Polynomial(coefs1);
+            Polynomial pol2 = new Polynomial(coefs2);
+            Polynomial polex = new Polynomial(coefsex);
+
+            Polynomial actual = pol1 - pol2;
+
+            Assert.True(polex == actual);
+
+        }
+
+        [TestCase(new double[] { 3, 6, 9 }, new double[] { 1, 2, 3 }, new double[] { 3, 12, 30, 36, 27 })]
+        [TestCase(new double[] { 37, 4, -10 }, new double[] { 14.5, 0, -5.5 }, new double[] { 536.5, 58, -348.5, -22, 55 })]
+        public void Multiply_DoubleArrays_ValidMultiply(double[] coefs1, double[] coefs2, double[] coefsex)
+        {
+            Polynomial pol1 = new Polynomial(coefs1);
+            Polynomial pol2 = new Polynomial(coefs2);
+            Polynomial polex = new Polynomial(coefsex);
+
+            Polynomial actual = pol1 * pol2;
+
+            Assert.True(polex == actual);
+
+        }
+
+        [Test]
+        public void ArgumentNullExceptionIfArrayEqualeNull() =>
+            Assert.Throws<ArgumentNullException>(() => new Polynomial(null));
+    }
+}
---
+++ NET.S.2018.Lyubchenko.05.06/Polynomial/Polynomial.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polynomial
+{
+    public class Polynomial
+    {
+        private double[] Coefs { get; set; }
+        private int Degree { get; set; }
+
+        public Polynomial(double[] coefs)
+        {
+            IsValid(coefs);
+            Coefs = coefs;
+            Degree = Coefs.Length - 1;
+        }
+
+        public override bool Equals(object polynomial)
+        {
+            if (polynomial != null)
+            {
+                Polynomial pol = polynomial as Polynomial;
+                return this == pol;
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            if (Coefs != null)
+            {
+                return Coefs.GetHashCode();
+            }
+
+            return 0;
+        }
+
+        private static Polynomial OperatorPlusHelper(Polynomial pol1, Polynomial pol2)
+        {
+            double[] doneCoefs = new double[pol1.Degree + 1];
+            for (int j = 0; j < pol1.Degree - pol2.Degree; j++)
+            {
+                doneCoefs[j] = pol1.Coefs[j];
+            }
+
+            for (int i = pol1.Degree - pol2.Degree; i <= pol1.Degree; i++)
+            {
+                doneCoefs[i] = pol1.Coefs[i] + pol2.Coefs[i - pol1.Degree + pol2.Degree];
+            }
+
+            Polynomial donePol = new Polynomial(doneCoefs);
+            return donePol;
+        }
+
+        private static Polynomial OperatorSubtractionHelper(Polynomial pol1, Polynomial pol2)
+        {
+            double[] doneCoefs = new double[pol1.Degree + 1];
+            for (int j = 0; j < pol1.Degree - pol2.Degree; j++)
+            {
+                doneCoefs[j] = pol1.Coefs[j];
+            }
+
+            for (int i = pol1.Degree - pol2.Degree; i <= pol1.Degree; i++)
+            {
+                doneCoefs[i] = pol1.Coefs[i] - pol2.Coefs[i - pol1.Degree + pol2.Degree];
+            }
+
+            Polynomial donePol = new Polynomial(doneCoefs);
+            return donePol;
+        }
+
+        public override string ToString()
+        {
+            string polynomyal = $"{Coefs[0]}x^{Degree}";
+            for (int i = 1; i < Degree - 1; i++)
+            {
+                if (Coefs[i] != 0.0)
+                {
+                    polynomyal += $" + {Coefs[i]}x^{Degree - i}";
+                }
+            }
+            if (Coefs[Degree - 1] != 0.0)
+            {
+                polynomyal += $" + {Coefs[Degree - 1]}x";
+            }
+
+            if (Coefs[Degree] != 0.0)
+            {
+                polynomyal += $" + {Coefs[Degree]}";
+            }
+            polynomyal += " = 0";
+            return polynomyal;
+        }
+
+
+        public static Polynomial operator +(Polynomial pol1, Polynomial pol2)
+        {
+            if (pol1.Degree == pol2.Degree)
+            {
+                double[] doneCoefs = new double[pol2.Degree + 1];
+                for (int i = 0; i <= pol1.Degree; i++)
+                {
+                    doneCoefs[i] = pol1.Coefs[i] + pol2.Coefs[i];
+                }
+
+                Polynomial donePol = new Polynomial(doneCoefs);
+                return donePol;
+            }
+            else if (pol1.Degree > pol2.Degree)
+            {
+                Polynomial donePol = OperatorPlusHelper(pol1, pol2);
+                return donePol;
+            }
+            else if (pol2.Degree > pol1.Degree)
+            {
+                Polynomial donePol = OperatorPlusHelper(pol2, pol1);
+                return donePol;
+            }
+
+            return null;
+        }
+
+        public static Polynomial operator -(Polynomial pol1, Polynomial pol2)
+        {
+            if (pol1.Degree == pol2.Degree)
+            {
+                double[] doneCoefs = new double[pol2.Degree + 1];
+                for (int i = 0; i <= pol1.Degree; i++)
+                {
+                    doneCoefs[i] = pol1.Coefs[i] - pol2.Coefs[i];
+                }
+
+                Polynomial donePol = new Polynomial(doneCoefs);
+                return donePol;
+            }
+            else if (pol1.Degree > pol2.Degree)
+            {
+                Polynomial donePol = OperatorSubtractionHelper(pol1, pol2);
+                return donePol;
+            }
+            else if (pol2.Degree > pol1.Degree)
+            {
+                Polynomial donePol = OperatorSubtractionHelper(pol2, pol1);
+                return donePol;
+            }
+
+            return null;
+        }
+
+        public static Polynomial operator *(Polynomial pol1, Polynomial pol2)
+        {
+            double[] doneCoefs = new double[pol1.Degree * pol2.Degree + 1];
+            for (int i = 0; i <= pol1.Degree; i++)
+            {
+                for (int j = 0; j <= pol2.Degree; j++)
+                {
+                    doneCoefs[i + j] += pol1.Coefs[i] * pol2.Coefs[j];
+                }
+            }
+
+            Polynomial donePol = new Polynomial(doneCoefs);
+            return donePol;
+        }
+
+        public static bool operator !=(Polynomial pol1, Polynomial pol2)
+        {
+            if (pol1.Degree == pol2.Degree)
+            {
+                for (int i = 0; i <= pol1.Degree; i++)
+                {
+                    if (pol1.Coefs[i] != pol2.Coefs[i])
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public static bool operator ==(Polynomial pol1, Polynomial pol2)
+        {
+            if (pol1.Degree == pol2.Degree)
+            {
+                for (int i = 0; i <= pol1.Degree; i++)
+                {
+                    if (pol1.Coefs[i] != pol2.Coefs[i])
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        private void IsValid(double[] coefs)
+        {
+            if (coefs == null || coefs.Length == 0)
+            {
+                throw new ArgumentNullException(nameof(coefs));
+            }
+        }
+    }
+}
