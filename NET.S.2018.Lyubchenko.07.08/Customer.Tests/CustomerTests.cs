310de5a452103afe7e720827934c8402ac642e5b
Akim <akim.lyubchenko@gmail.com>
2018-07-11 21:58:33 +0300

    Customer

---
+++ NET.S.2018.Lyubchenko.07.08/Customer.Tests/CustomerTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Customer.Tests
+{
+    [TestFixture]
+    public class CustomerTests
+    {
+        [TestCase("nrcp", ExpectedResult = "Jeffrey Richter, 1,000,000.00, +1 (425) 555-0100")]
+        [TestCase("ncp", ExpectedResult = "Jeffrey Richter, +1 (425) 555-0100")]
+        [TestCase("n", ExpectedResult = "Jeffrey Richter")]
+        [TestCase("r", ExpectedResult = "1,000,000.00")]
+        public string Customer_SomeCondition_Result(string format)
+        {
+            Customer customer = new Customer("Jeffrey Richter", "+1 (425) 555-0100", 1000000);
+            return customer.ToString(format);
+        }
+    }
+}
---
+++ NET.S.2018.Lyubchenko.07.08/Customer/Customer.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Customer
+{
+    public class Customer : IFormattable
+    {
+        public string Name
+        {
+            get;
+            private set;
+        }
+
+        public string ContactPhone
+        {
+            get;
+            private set;
+        }
+
+        public decimal Revenue
+        {
+            get;
+            private set;
+        }
+
+        public Customer(string name, string contactPhone, decimal revenue)
+        {
+            Validator(name, contactPhone, revenue);
+            Name = name;
+            ContactPhone = contactPhone;
+            Revenue = revenue;
+        }
+        public override string ToString()
+            => $"{Name} {ContactPhone} {Revenue}";
+
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            if (string.IsNullOrEmpty(format))
+            {
+                format = "nrcp";
+            }
+
+            return IFormatProviderToString(format.ToLowerInvariant(), formatProvider);
+        }
+
+        public string ToString(string format)
+            => ToString(format, CultureInfo.InvariantCulture);
+
+        private string IFormatProviderToString(string format, IFormatProvider formatProvider)
+        {
+            switch (format)
+            {
+                case "nrcp":
+                    return $"{Name}, {Revenue.ToString("n", formatProvider)}, {ContactPhone}";
+                case "n":
+                    return $"{Name}";
+                case "cp":
+                    return $"{ContactPhone}";
+                case "r":
+                    return $"{Revenue.ToString("n", formatProvider)}";
+                case "ncp":
+                    return $"{Name}, {ContactPhone}";
+                case "nr":
+                    return $"{Name}, {Revenue.ToString("n", formatProvider)}";
+                case "rcp":
+                    return $"{Revenue.ToString("n", formatProvider)}, {ContactPhone}";
+                default:
+                    throw new FormatException(nameof(format));
+            }
+        }
+
+        private void Validator(string name, string contactPhone, decimal revenue)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentException($"{nameof(name)} must contain name");
+            }
+
+            if (!Regex.IsMatch(name , @"^([A-Z][a-z]*\s)*[A-Z][a-z]*$"))
+            {
+                throw new FormatException($"{nameof(name)} must have only symbols a(A) - z(Z)");
+            }
+
+            if (string.IsNullOrEmpty(contactPhone))
+            {
+                throw new ArgumentException($"{nameof(contactPhone)} must contain phone");
+            }
+
+            if (revenue < 0)
+            {
+                throw new ArgumentException($"{nameof(revenue)} must be positive");
+            }
+        }
+    }
+}
