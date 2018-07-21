e16e3bfa4c83d464a65d51d66330452b23a787b2
Akim <akim.lyubchenko@gmail.com>
2018-07-21 14:19:22 +0300

    Refactored Jugged Array and clean folders

--- NET.S.2018.Lyubchenko.09/BubbleJaggedSort/BubbleJaggedSort.cs
+++ NET.S.2018.Lyubchenko.09/BubbleJaggedSort/BubbleJaggedSort.cs
@@ -16,190 +16,37 @@ namespace BubbleJaggedSort
         /// <exception cref="System.ArgumentNullException">
         /// Appears if any array == null
         /// </exception>
-        public static int[][] BubbleSort(int[][] array, int format = 1)
+        public static void BubbleSort(int[][] array, Comparison<int[]> comp)
+        {
+            Validator(array, comp);
+            bool temp = false;
+            while (!temp)
+            {
+                temp = true;
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    if (comp.Invoke(array[i], array[i + 1]) > 0)
+                    {
+                        Swap(ref array[i], ref array[i + 1]);
+                        temp = false;
+                    }
+                }
+            }
+        }
+
+        private static void Validator(int[][] array, Comparison<int[]> comp)
         {
             if (array == null)
             {
-                throw new ArgumentNullException(nameof(array));
+                throw new ArgumentNullException($"{nameof(array)} must contain numbers");
             }
 
-            for (int i = 0; i < array.Length; i++)
+            if (comp == null)
             {
-                if (array[i] == null)
-                {
-                    throw new ArgumentNullException("Arrays must contain any numbers");
-                }
-            }
-
-            switch (format)
-            {
-                case 1:
-                    return SumSort(array);
-                case 2:
-                    return MaxElSort(array);
-                case 3:
-                    return Inverse(MinElSort(array));
-                case 4:
-                    return Inverse(SumSort(array));
-                case 5:
-                    return Inverse(MaxElSort(array));
-                case 6:
-                    return MinElSort(array);
-                default:
-                    return SumSort(array);
+                throw new ArgumentNullException($"Transfer method");
             }
         }
 
-        /// <summary>
-        /// Sort array
-        /// </summary>
-        /// <param name="array">The array</param>
-        /// <param name="tempArray">The temporary array.</param>
-        private static void BubbleSort(int[][] array, int[] tempArray)
-        {
-            int temp = -1;
-            while (temp != 0)
-            {
-                temp = 0;
-                for (int i = 0; i < array.Length - 1; i++)
-                {
-                    if (tempArray[i] > tempArray[i + 1])
-                    {
-                        Swap(ref tempArray[i], ref tempArray[i + 1]);
-                        Swap(ref array[i], ref array[i + 1]);
-                        temp++;
-                    }
-                }
-            }
-        }
-
-        /// <summary>
-        /// Sums the array
-        /// </summary>
-        /// <param name="array">The array.</param>
-        /// <returns> Sorted array </returns>
-        private static int[][] SumSort(int[][] array)
-        {
-            BubbleSort(array, SumHelper(array));
-            return array;
-        }
-
-        /// <summary>
-        /// Sums the helper.
-        /// </summary>
-        /// <param name="array">The array.</param>
-        /// <returns> array width sum elements </returns>
-        private static int[] SumHelper(int[][] array)
-        {
-            int[] sumTemp = new int[array.Length];
-            for (int i = 0; i < array.Length; i++)
-            {
-                for (int j = 0; j < array[i].Length; j++)
-                {
-                    sumTemp[i] += array[i][j];
-                }
-            }
-
-            return sumTemp;
-        }
-
-        /// <summary>
-        /// Max element in array 
-        /// </summary>
-        /// <param name="array">The array.</param>
-        /// <returns> Sorted array </returns>
-        private static int[][] MaxElSort(int[][] array)
-        {
-            BubbleSort(array, MaxElHelper(array));
-            return array;
-        }
-
-        /// <summary>
-        /// array of max el-s
-        /// </summary>
-        /// <param name="array">The array.</param>
-        /// <returns> array of max el-s </returns>
-        private static int[] MaxElHelper(int[][] array)
-        {
-            int[] maxElTemp = new int[array.Length];
-            for (int i = 0; i < array.Length; i++)
-            {
-                maxElTemp[i] = array[i][0];
-                for (int j = 0; j < array[i].Length - 1; j++)
-                {
-                    if (maxElTemp[i] < array[i][j + 1])
-                    {
-                        maxElTemp[i] = array[i][j + 1];
-                    }
-                }
-            }
-
-            return maxElTemp;
-        }
-
-        /// <summary>
-        /// Min elements in arrays 
-        /// </summary>
-        /// <param name="array">The array.</param>
-        /// <returns> Sorted array </returns>
-        private static int[][] MinElSort(int[][] array)
-        {
-            BubbleSort(array, MaxElHelper(MinElHelper(array)));
-            return array;
-        }
-
-        /// <summary>
-        /// Temp array width negative elements
-        /// </summary>
-        /// <param name="array">The array.</param>
-        /// <returns> Temp array width negative elements </returns>
-        private static int[][] MinElHelper(int[][] array)
-        {
-            int[][] tempArr = new int[array.Length][];
-            for (int i = 0; i < array.Length; i++)
-            {
-                tempArr[i] = new int[array[i].Length];
-                for (int j = 0; j < array[i].Length; j++)
-                {
-                    tempArr[i][j] = array[i][j] * (-1);
-                }
-            }
-
-            return tempArr;
-        }
-
-        /// <summary>
-        /// Inverses the specified array.
-        /// </summary>
-        /// <param name="array">The array.</param>
-        /// <returns> Convert array </returns>
-        private static int[][] Inverse(int[][] array)
-        {
-            for (int i = 0; i < array.Length / 2; i++)
-            {
-                Swap(ref array[i], ref array[array.Length - i - 1]);
-            }
-
-            return array;
-        }
-
-        /// <summary>
-        /// Swaps the specified integers.
-        /// </summary>
-        /// <param name="lhs">The first int.</param>
-        /// <param name="rhs">The second int.</param>
-        private static void Swap(ref int lhs, ref int rhs)
-        {
-            int temp = rhs;
-            rhs = lhs;
-            lhs = temp;
-        }
-
-        /// <summary>
-        /// Swaps the specified arrays.
-        /// </summary>
-        /// <param name="lhs">The first array.</param>
-        /// <param name="rhs">The second array.</param>
         private static void Swap(ref int[] lhs, ref int[] rhs)
         {
             int[] temp = rhs;
---
+++ NET.S.2018.Lyubchenko.09/BubbleJaggedSort/Conditions.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditions
+{
+    /// <summary>
+    /// Sum 2 arrays
+    /// </summary>
+    /// <seealso cref="System.Collections.Generic.IComparer{System.Int32[]}" />
+    public class Sum : IComparer<int[]>
+    {
+        /// <summary>
+        /// Compares 2 arrays
+        /// </summary>
+        /// <param name="lhs">The LHS.</param>
+        /// <param name="rhs">The RHS.</param>
+        /// <returns> Positive if lhs > 0 </returns>
+        public int Compare(int[] lhs, int[] rhs)
+        {
+            if (ReferenceEquals(lhs, rhs))
+            {
+                return 0;
+            }
+
+            if (lhs == null)
+            {
+                return -1;
+            }
+
+            if (rhs == null)
+            {
+                return 1;
+            }
+
+            return Helpers.SumHelper(lhs) - Helpers.SumHelper(rhs);
+        }
+    }
+
+    /// <summary>
+    /// InverseSum 2 arrays
+    /// </summary>
+    /// <seealso cref="System.Collections.Generic.IComparer{System.Int32[]}" />
+    public class InverseSum : IComparer<int[]>
+    {
+        /// <summary>
+        /// Compares 2 arrays
+        /// </summary>
+        /// <param name="lhs">The LHS.</param>
+        /// <param name="rhs">The RHS.</param>
+        /// <returns> Positive if lhs > 0 </returns>
+        public int Compare(int[] lhs, int[] rhs)
+        {
+            if (ReferenceEquals(lhs, rhs))
+            {
+                return 0;
+            }
+
+            if (lhs == null)
+            {
+                return 1;
+            }
+
+            if (rhs == null)
+            {
+                return -1;
+            }
+
+            return Helpers.SumHelper(rhs) - Helpers.SumHelper(lhs);
+        }
+    }
+
+    /// <summary>
+    /// Min el of 2 arrays
+    /// </summary>
+    /// <seealso cref="System.Collections.Generic.IComparer{System.Int32[]}" />
+    public class Min : IComparer<int[]>
+    {
+        /// <summary>
+        /// Compares 2 arrays
+        /// </summary>
+        /// <param name="lhs">The LHS.</param>
+        /// <param name="rhs">The RHS.</param>
+        /// <returns> Positive if lhs > 0 </returns>
+        public int Compare(int[] lhs, int[] rhs)
+        {
+            if (ReferenceEquals(lhs, rhs))
+            {
+                return 0;
+            }
+
+            if (lhs == null)
+            {
+                return -1;
+            }
+
+            if (rhs == null)
+            {
+                return 1;
+            }
+
+            return Helpers.MinHelper(lhs) - Helpers.MinHelper(rhs);
+        }
+    }
+
+    /// <summary>
+    /// InverseMin el for 2 arrays
+    /// </summary>
+    /// <seealso cref="System.Collections.Generic.IComparer{System.Int32[]}" />
+    public class InverseMin : IComparer<int[]>
+    {
+        /// <summary>
+        /// Compares 2 arrays
+        /// </summary>
+        /// <param name="lhs">The LHS.</param>
+        /// <param name="rhs">The RHS.</param>
+        /// <returns> Positive if lhs > 0 </returns>
+        public int Compare(int[] lhs, int[] rhs)
+        {
+            if (ReferenceEquals(lhs, rhs))
+            {
+                return 0;
+            }
+
+            if (lhs == null)
+            {
+                return 1;
+            }
+
+            if (rhs == null)
+            {
+                return -1;
+            }
+
+            return Helpers.MinHelper(rhs) - Helpers.MinHelper(lhs);
+        }
+    }
+
+    /// <summary>
+    /// Max el of 2 arrays
+    /// </summary>
+    /// <seealso cref="System.Collections.Generic.IComparer{System.Int32[]}" />
+    public class Max : IComparer<int[]>
+    {
+        /// <summary>
+        /// Compares 2 arrays
+        /// </summary>
+        /// <param name="lhs">The LHS.</param>
+        /// <param name="rhs">The RHS.</param>
+        /// <returns> Positive if lhs > 0 </returns>
+        public int Compare(int[] lhs, int[] rhs)
+        {
+            if (ReferenceEquals(lhs, rhs))
+            {
+                return 0;
+            }
+
+            if (lhs == null)
+            {
+                return -1;
+            }
+
+            if (rhs == null)
+            {
+                return 1;
+            }
+
+            return Helpers.MaxHelper(lhs) - Helpers.MaxHelper(rhs);
+        }
+    }
+
+    /// <summary>
+    /// Inverse max of 2 arrays
+    /// </summary>
+    /// <seealso cref="System.Collections.Generic.IComparer{System.Int32[]}" />
+    public class InverseMax : IComparer<int[]>
+    {
+        /// <summary>
+        /// Compares 2 arrays
+        /// </summary>
+        /// <param name="lhs">The LHS.</param>
+        /// <param name="rhs">The RHS.</param>
+        /// <returns> Positive if lhs > 0 </returns>
+        public int Compare(int[] lhs, int[] rhs)
+        {
+            if (ReferenceEquals(lhs, rhs))
+            {
+                return 0;
+            }
+
+            if (lhs == null)
+            {
+                return 1;
+            }
+
+            if (rhs == null)
+            {
+                return -1;
+            }
+
+            return Helpers.MaxHelper(rhs) - Helpers.MaxHelper(lhs);
+        }
+    }
+
+    /// <summary>
+    /// Helpers for operations
+    /// </summary>
+    internal static class Helpers
+    {
+        internal static int MaxHelper(int[] array)
+        {
+            int max = 0;
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (max < array[i])
+                {
+                    max = array[i];
+                }
+            }
+
+            return max;
+        }
+
+        internal static int MinHelper(int[] array)
+        {
+            int min = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (min > array[i])
+                {
+                    min = array[i];
+                }
+            }
+
+            return min;
+        }
+
+        internal static int SumHelper(int[] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+
+            return sum;
+        }
+    }
+}
