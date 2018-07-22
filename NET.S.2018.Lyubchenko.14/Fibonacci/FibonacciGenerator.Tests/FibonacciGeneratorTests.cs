f547592117a7b0263f548e059c6ae4d45cd30b1e
Akim <akim.lyubchenko@gmail.com>
2018-07-22 18:21:56 +0300

    Play width GitHub

---
+++ NET.S.2018.Lyubchenko.14/Fibonacci/FibonacciGenerator.Tests/FibonacciGeneratorTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using FibonacciGenereator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fibonacci.Tests
+{
+    [TestClass]
+    public class FibonacciTests
+    {
+        [TestMethod]
+        public void GeneratorFibonacci_3_DoneArray()
+        {
+            int size = 3;
+            int[] expected = new int[] { 1, 1, 2 };
+
+            int[] actual = FibonacciGenerator.GeneratorFibonacci(size);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GeneratorFibonacci_7_DoneArray()
+        {
+            int size = 7;
+            int[] expected = new int[] { 1, 1, 2, 3, 5, 8, 13 };
+
+            int[] actual = FibonacciGenerator.GeneratorFibonacci(size);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GeneratorFibonacci_11_DoneArray()
+        {
+            int size = 11;
+            int[] expected = new int[] { 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89 };
+
+            int[] actual = FibonacciGenerator.GeneratorFibonacci(size);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GeneratorFibonacci_Exception()
+            => FibonacciGenerator.GeneratorFibonacci(0);
+    }
+}
---
+++ NET.S.2018.Lyubchenko.14/Fibonacci/FibonacciGenerator/FibonacciGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FibonacciGenereator
+{
+    /// <summary>
+    /// Generator Fibonacci numbers
+    /// </summary>
+    public static class FibonacciGenerator
+    {
+        /// <summary>
+        /// Generator Fibonacci numbers
+        /// </summary>
+        /// <param name="size">The size of array</param>
+        /// <returns>Array width Fibonacci numbers</returns>
+        /// <exception cref="System.ArgumentException"></exception>
+        public static int[] GeneratorFibonacci(int size)
+        {
+            if (size < 1)
+            {
+                throw new ArgumentException($"Write size > 0");
+            }
+
+            int[] fib = new int[size];
+            int value = 1;
+            fib[0] = value;
+            for (int i = 1; i < size; i++)
+            {
+                fib[i] = value;
+                value += fib[i - 1];
+            }
+            return fib;
+        }
+    }
+}
